c95659e47bc7b8929932ea0dbab7947425213f6d
mdemski <m.demski@outlook.com>
2019-10-22 20:09:59 +0200

    New packages have been added.

--- src/main/java/pl/coderslab/charity/app/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/app/CharityApplication.java
@@ -1,4 +1,4 @@
-package pl.coderslab.charity;
+package pl.coderslab.charity.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
