b993093b7111c8a3bf00607097eec52299dce7c0
mdemski <m.demski@outlook.com>
2019-10-23 21:16:18 +0200

    Added 3 models to jsp view

--- src/main/java/pl/coderslab/charity/web/controllers/HomeController.java
+++ src/main/java/pl/coderslab/charity/web/controllers/HomeController.java
@@ -3,14 +3,35 @@ package pl.coderslab.charity.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Donation;
+import pl.coderslab.charity.repositories.DonationRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
+
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
+        this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
+    }
 
     @RequestMapping("/")
-    public String homeAction(Model model){
+    public String homeAction(Model model) {
+        model.addAttribute("institutions", institutionRepository.findAll());
+        List<Donation> donationList = donationRepository.findAll();
+        int bags = 0;
+        for (Donation donation : donationList) {
+            bags += donation.getQuantity();
+        }
+        model.addAttribute("bags", bags);
+        long supported = donationList.stream().map(Donation::getInstitution).count();
+        model.addAttribute("supportedInstitutions", supported);
         return "index";
     }
 }
