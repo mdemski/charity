5ec2b81438be7a1b38be5394282375e9bd30507d
mdemski <m.demski@outlook.com>
2019-10-27 11:14:23 +0100

    Data for donation form have been added

--- src/main/java/pl/coderslab/charity/web/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/web/controllers/DonationController.java
@@ -1,11 +1,29 @@
 package pl.coderslab.charity.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.repositories.CategoryRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
 
 @Controller
-@RequestMapping("datek/")
+@RequestMapping("/datek")
 public class DonationController {
 
+    private CategoryRepository categoryRepository;
+    private InstitutionRepository institutionRepository;
+
+    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository) {
+        this.categoryRepository = categoryRepository;
+        this.institutionRepository = institutionRepository;
+    }
+
+    @GetMapping
+    public String prepareDonationFormPage(Model model){
+        model.addAttribute("categories", categoryRepository.findAll());
+        model.addAttribute("institutions", institutionRepository.findAll());
+        return "donationForm";
+    }
 
 }
