7da897dbc73fca6ae1649016b286d692511b17a1
mdemski <m.demski@outlook.com>
2019-10-27 11:20:42 +0100

    Native query to get all bags with donation has been added

--- src/main/java/pl/coderslab/charity/repositories/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repositories/DonationRepository.java
@@ -1,8 +1,11 @@
 package pl.coderslab.charity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.AbstractEntity;
 import pl.coderslab.charity.model.Donation;
 
-public interface DonationRepository <T extends AbstractEntity, L extends Number> extends JpaRepository<Donation, Long> {
+public interface DonationRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Donation, Long> {
+    @Query("SELECT SUM (qunatity) FROM donations")
+    int sumAllBags();
 }
