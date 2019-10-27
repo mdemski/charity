8bc82f66fcc965637b8db4949480a0fa1d49b05e
mdemski <m.demski@outlook.com>
2019-10-27 11:19:36 +0100

    All supported institutions native query has been added

--- src/main/java/pl/coderslab/charity/repositories/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/repositories/InstitutionRepository.java
@@ -1,8 +1,13 @@
 package pl.coderslab.charity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.AbstractEntity;
 import pl.coderslab.charity.model.Institution;
 
-public interface InstitutionRepository <T extends AbstractEntity, L extends Number> extends JpaRepository<Institution, Long> {
+import java.util.List;
+
+public interface InstitutionRepository<T extends AbstractEntity, L extends Number> extends JpaRepository<Institution, Long> {
+    @Query("SELECT DISTINCT i FROM institutions")
+    List<Institution> getAllSupportedInstitutions();
 }
