93dfd56c381467bb8fe9d57d8cf00cd82ce9be29
mdemski <m.demski@outlook.com>
2019-10-27 18:32:18 +0100

    Created new objects, reconfigured pushing data to jsp views.

--- src/main/java/pl/coderslab/charity/web/controllers/HomeController.java
+++ src/main/java/pl/coderslab/charity/web/controllers/HomeController.java
@@ -3,10 +3,15 @@ package pl.coderslab.charity.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.model.Category;
 import pl.coderslab.charity.model.Donation;
+import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repositories.CategoryRepository;
 import pl.coderslab.charity.repositories.DonationRepository;
 import pl.coderslab.charity.repositories.InstitutionRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -15,23 +20,26 @@ public class HomeController {
 
     private InstitutionRepository institutionRepository;
     private DonationRepository donationRepository;
+    private CategoryRepository categoryRepository;
 
-    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository, CategoryRepository categoryRepository) {
         this.institutionRepository = institutionRepository;
         this.donationRepository = donationRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model) {
+        Institution institution = new Institution("Dla dobra", "Szykujemy worki dla potrzebujących");
+        institutionRepository.save(institution);
+        Category category = new Category("clothes");
+        categoryRepository.save(category);
+        List<Category> categories = categoryRepository.findAll();
+        Donation donation = new Donation(1, categories, institution, "Wrocławska", "Opole", "45-000", LocalDate.now(), LocalDateTime.now(), "Oddane na potrzeby dzieci");
+        donationRepository.save(donation);
         model.addAttribute("institutions", institutionRepository.findAll());
-        List<Donation> donationList = donationRepository.findAll();
-        int bags = 0;
-        for (Donation donation : donationList) {
-            bags += donation.getQuantity();
-        }
-        model.addAttribute("bags", bags);
-        long supported = donationList.stream().map(Donation::getInstitution).count();
-        model.addAttribute("supportedInstitutions", supported);
+        model.addAttribute("bags", donationRepository.sumAllBags());
+        model.addAttribute("supportedInstitutions", institutionRepository.getAllSupportedInstitutions());
         return "index";
     }
 }
