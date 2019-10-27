8e40a107f96d26d7fa4b8f52008b49dcd0055195
mdemski <m.demski@outlook.com>
2019-10-27 20:20:59 +0100

    CharityApplication java file moved form app folder

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -1,4 +1,4 @@
-package pl.coderslab.charity.app;
+package pl.coderslab.charity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
