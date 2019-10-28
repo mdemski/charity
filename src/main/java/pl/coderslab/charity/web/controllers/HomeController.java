c72523fa8cc871ef19df3bcb0488728fa9a04c08
mdemski <m.demski@outlook.com>
2019-10-28 19:52:59 +0100

    Adding data to database has been deleted. Rebuilt attribute with number of supported institutions

--- src/main/java/pl/coderslab/charity/web/controllers/HomeController.java
+++ src/main/java/pl/coderslab/charity/web/controllers/HomeController.java
@@ -3,15 +3,11 @@ package pl.coderslab.charity.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.charity.model.Category;
-import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.model.Institution;
 import pl.coderslab.charity.repositories.CategoryRepository;
 import pl.coderslab.charity.repositories.DonationRepository;
 import pl.coderslab.charity.repositories.InstitutionRepository;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -20,26 +16,18 @@ public class HomeController {
 
     private InstitutionRepository institutionRepository;
     private DonationRepository donationRepository;
-    private CategoryRepository categoryRepository;
 
-    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository, CategoryRepository categoryRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
         this.donationRepository = donationRepository;
-        this.categoryRepository = categoryRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model) {
-        Institution institution = new Institution("Dla dobra", "Szykujemy worki dla potrzebujących");
-        institutionRepository.save(institution);
-        Category category = new Category("clothes");
-        categoryRepository.save(category);
-        List<Category> categories = categoryRepository.findAll();
-        Donation donation = new Donation(1, categories, institution, "Wrocławska", "Opole", "45-000", LocalDate.now(), LocalDateTime.now(), "Oddane na potrzeby dzieci");
-        donationRepository.save(donation);
         model.addAttribute("institutions", institutionRepository.findAll());
         model.addAttribute("bags", donationRepository.sumAllBags());
-        model.addAttribute("supportedInstitutions", institutionRepository.getAllSupportedInstitutions());
+        List<Institution> institutions = institutionRepository.getAllSupportedInstitutions();
+        model.addAttribute("supportedInstitutions", institutions.size());
         return "index";
     }
 }
