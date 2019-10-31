c1bb7bb9359e8df26465072ed9313d994d70a0d3
mdemski <m.demski@outlook.com>
2019-10-31 19:16:43 +0100

    Getter and setter have been added

--- src/main/java/pl/coderslab/charity/model/AbstractEntity.java
+++ src/main/java/pl/coderslab/charity/model/AbstractEntity.java
@@ -25,4 +25,12 @@ public abstract class AbstractEntity {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
