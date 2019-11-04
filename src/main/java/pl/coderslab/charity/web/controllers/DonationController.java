b90d83e721fb02d8fdecd3dd38b5bd7f9028e4f6
mdemski <m.demski@outlook.com>
2019-11-04 20:02:22 +0100

    Added request mapping and post method to save donation in database

--- src/main/java/pl/coderslab/charity/web/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/web/controllers/DonationController.java
@@ -3,29 +3,40 @@ package pl.coderslab.charity.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.model.Donation;
 import pl.coderslab.charity.repositories.CategoryRepository;
+import pl.coderslab.charity.repositories.DonationRepository;
 import pl.coderslab.charity.repositories.InstitutionRepository;
 
-@Controller
 
+@Controller
+@RequestMapping("/datek")
 public class DonationController {
 
     private CategoryRepository categoryRepository;
     private InstitutionRepository institutionRepository;
+    private DonationRepository donationRepository;
 
-    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository) {
+    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.categoryRepository = categoryRepository;
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
-    @RequestMapping("/datek")
+    @GetMapping
     public String prepareDonationFormPage(Model model){
         model.addAttribute("donation", new Donation());
         model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("institutions", institutionRepository.findAll());
         return "donationForm";
+    }
+
+    @PostMapping
+    public String processDonationFormPage(Donation donation){
+        donationRepository.save(donation);
+        return "redirect:/potwierdzenie";
     }
 
 }
