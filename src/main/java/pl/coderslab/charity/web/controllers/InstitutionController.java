08c258b865fd8d22d2fcaee194f4916eb3d9427c
mdemski <m.demski@outlook.com>
2019-11-12 17:16:21 +0100

    Institution controller

---
+++ src/main/java/pl/coderslab/charity/web/controllers/InstitutionController.java
@@ -0,0 +1,24 @@
+package pl.coderslab.charity.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.repositories.InstitutionRepository;
+
+@Controller
+@RequestMapping("/instytucje")
+public class InstitutionController {
+
+    private InstitutionRepository institutionRepository;
+
+    public InstitutionController(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+    @GetMapping
+    public String institutionManagePage(Model model){
+        model.addAttribute("institutions", institutionRepository.findAll());
+        return "institutions";
+    }
+}
